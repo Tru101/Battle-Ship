679c3f98bdd9a833d3d8116816f20a728d833882
Tru101 <truman.metz@gmail.com>
2019-01-31 17:49:32 -0500

    Add files via upload

---
+++ Battle ship.py
@@ -0,0 +1,8 @@
+import random as computer_call
+
+def computer_ships(x,y):
+   z = x + y
+   ship_list.append(z)
+ship_list = []
+row_List = ['A','B','C','D','E','F','G','H','I','J']
+column_List = [1,2,3,4,5,6,7,8,9,10]
