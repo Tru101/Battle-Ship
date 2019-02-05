8404aa3d40294c811205a65f8c6683f8b718c850
Tru101 <truman.metz@gmail.com>
2019-02-05 15:22:28 -0500

    Add files via upload

--- Battle ship.py
+++ Battle ship.py
@@ -1,8 +1,78 @@
 import random as computer_call
 
-def computer_ships(x,y):
+def game_board(x,y):
    z = x + y
-   ship_list.append(z)
-ship_list = []
+   return(z)
+
 row_List = ['A','B','C','D','E','F','G','H','I','J']
-column_List = [1,2,3,4,5,6,7,8,9,10]
+column_List = ['1','2','3','4','5','6','7','8','9','10','11']
+game_boardplayer = []
+game_boardcomputer = []
+game_board_available = []
+moves_called = []
+listcounter1 = 0
+listcounter2 = 0
+listcounter3 = 0
+listcounter4 = 0
+
+preset_player1 = ['A1','A2','A3','A4','A5','C10','D10','E10','F10','H5','H6','H7','J3','J4','E7','F7','G8','H1' ]
+preset_computer = ['A5','B5','C5','D5','E5','G6','G7','G8','G9','E2','F2','G2', 'I4','I5','C9','C10','I9','J1']
+
+
+for i in range (1,101):
+    column_number = column_List[listcounter1]
+    if column_number == '11':
+        listcounter2 = listcounter2 + 1 
+        column_number = '1'
+        listcounter1 = 0
+    row_letter = row_List[listcounter2]
+    game_boardplayer.append(game_board(row_letter,column_number))
+    game_boardcomputer.append(game_board(row_letter,column_number))
+    game_board_available.append(game_board(row_letter,column_number))
+    listcounter1 = listcounter1 + 1
+    
+    
+for i in range(1,19):
+   i = listcounter3
+   listcounter3 = listcounter3 + 1
+   if preset_player1[i] in game_boardplayer:
+       print(preset_player1[i])
+       game_boardplayer.remove(preset_player1[i])
+
+for i in range(1,19):
+   i = listcounter4
+   listcounter4 = listcounter4 + 1
+   if preset_computer[i] in game_boardcomputer:
+       print(preset_computer[i])
+       game_boardcomputer.remove(preset_computer[i])      
+
+print(game_boardplayer)#Board with values that determine a miss for the computer
+print(game_boardcomputer)#Board with values that determine a miss for the player
+print(game_board_available)#Board with avilable spaces to make a move 
+
+
+for i in range(1,1000):
+    player_move = input("Players turn! Please select a Letter from (Capital letter Please) A-J and a number 1-10 to make your attack. If you want to see the moves you have already called input Z11")
+    moves_called.append(player_move)
+    if player_move == 'Z11':
+        print(moves_called)
+    if player_move in preset_computer:
+        print("HIT!!")
+        preset_computer.remove(player_move)
+    print(preset_computer)    
+    if player_move in game_boardcomputer:
+        print("Miss")
+        game_boardcomputer.remove(player_move)
+        
+    x = computer_call.randint(0,100)
+    computer_move = game_board_available[x]
+    print(computer_move)
+    if computer_move in preset_player1:
+        print("HIT!!")
+        preset_player1.remove(computer_move)
+    if computer_move in game_boardplayer:
+        print("Miss")
+          
+    if preset_player1 == [] or preset_computer == []:
+        print("Game is over")
+        break
